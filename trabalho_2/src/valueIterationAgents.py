1090541a41293ea2748ab02d9c8f44e87eee72b4
Arthur Ferreira Ely <arthur.ely78@gmail.com>
2024-05-25 01:30:01 -0300

    questão 1 e 2 feitas

--- trabalho_2/src/valueIterationAgents.py
+++ trabalho_2/src/valueIterationAgents.py
@@ -41,28 +41,39 @@ class ValueIterationAgent(ValueEstimationAgent):
         
         self.mdp = mdp
         self.discount = discount    #fator de desconto (gamma)
+        print(f"discount: {self.discount}")
         self.iterations = iterations #numero de vezes que o alguritmo de interação de valor sera executado
         self.values = util.Counter()  # A Counter is a dict with default 0
-
-        print("aaa")
 
         print(f"MDP type: {type(self.mdp)}")
         print(f"Discount type: {type(self.discount)}")
         print(f"Iterations type: {type(self.iterations)}")
         print(f"Values type: {type(self.values)}")
 
-        # Para ver os valores iniciais das variáveis
-        print(f"Initial values: {self.values}")
-
         # Write value iteration code here
         "*** YOUR CODE HERE ***"
-
+        for i in range(self.iterations):
+            new_values = util.Counter()
+            for state in self.mdp.getStates():
+                if self.mdp.isTerminal(state):
+                    new_values[state] = 0
+                else:
+                    max_q_value = float('-inf')
+                    for action in self.mdp.getPossibleActions(state):
+                        q_value = self.computeQValueFromValues(state, action)
+                        if q_value > max_q_value:
+                            max_q_value = q_value
+                    new_values[state] = max_q_value
+            self.values = new_values
+        
+        # Para ver os valores iniciais das variáveis
+        print(f"Initial values: {self.values}")
 
     def getValue(self, state):
         """
           Return the value of the state (computed in __init__).
         """
-
+        # print(f"self.values: {self.values}")
         return self.values[state]
 
     def computeQValueFromValues(self, state, action):
@@ -70,12 +81,12 @@ class ValueIterationAgent(ValueEstimationAgent):
           Compute the Q-value of action in state from the
           value function stored in self.values.
         """
-
-        for next_state, prob in self.mdp.getTransitionStatesAndProbs(state, action):
-            sum += prob*self.discount*self.getValue(self, state)
-
         
-        util.raiseNotDefined()
+        q_value = 0
+        for next_state, prob in self.mdp.getTransitionStatesAndProbs(state, action):
+            reward = self.mdp.getReward(state, action, next_state)
+            q_value += prob * (reward + self.discount * self.getValue(next_state))
+        return q_value
 
     #retorna melhor ação
     def computeActionFromValues(self, state):
